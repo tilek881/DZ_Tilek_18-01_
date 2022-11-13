4a140d07b80d7d752225ddf7634843b2ffedb78f
tilek881 <113850223+tilek881@users.noreply.github.com>
2022-11-13 23:29:40 +0600

    Add files via upload

---
+++ DZ1_Tilek_18-01/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+
+function  AboutUs () {
+    return (
+        <div>
+            <h1> AboutUs</h1>
+        </div>
+
+    )
+}
+function Portfolio () {
+    return(
+        <div>
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <AboutUs></AboutUs>
+        <Portfolio></Portfolio>
+    </div>
+  );
+}
+
+export default App;
